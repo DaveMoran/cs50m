a12faed4a02813081bddc255361b2d203dba8d1a
Dave Moran <dave.moran118@gmail.com>
2021-03-22 20:49:23 -0400

    Add unchecked update function

--- project0/script.js
+++ project0/script.js
@@ -8,10 +8,9 @@ const classNames = {
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
+const checkboxes = list.getElementsByClassName(classNames.TODO_CHECKBOX);
 
 function newTodo() {
-  console.log('New TODO button clicked!');
-
   let newHTML = `
   <li class="${classNames.TODO_ITEM}">
     <input class="${classNames.TODO_CHECKBOX}" type="checkbox" /> <input class="${classNames.TODO_TEXT}" type="text" />
@@ -19,8 +18,17 @@ function newTodo() {
 
   list.insertAdjacentHTML('beforeend', newHTML);
   updateItemCount();
+  updateUncheckedCount();
 }
 
 function updateItemCount() {
   itemCountSpan.innerText = list.childElementCount;
+}
+
+function updateUncheckedCount() {
+  let count = 0;
+  Array.from(checkboxes).forEach((item) => {
+    if(!item.checked) { count++ }
+  });
+  uncheckedCountSpan.innerText = count;
 }
