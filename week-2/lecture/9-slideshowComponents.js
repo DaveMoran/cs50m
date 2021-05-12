31f1f4cb1959eb691f6fe8ba3faa559883f73e8b
Dave Moran <dave.moran118@gmail.com>
2021-05-12 14:26:29 -0400

    fix type

--- week-2/lecture/9-slideshowComponents.js
+++ week-2/lecture/9-slideshowComponents.js
@@ -8,7 +8,7 @@ const Slide = slide => (
   <div>
     <h1>{slide.title}</h1>
     <ul>
-      {slide.bullets,map(bullet => <li>{bullet}</li>)}
+      {slide.bullets.map(bullet => <li>{bullet}</li>)}
     </ul>
   </div>
 )
