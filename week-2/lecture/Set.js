d4a54133ff0afe2bd16cf86a7b2e6356efc97450
Dave Moran <dave.moran118@gmail.com>
2021-05-12 13:42:35 -0400

    Add delete and size items

--- week-2/lecture/Set.js
+++ week-2/lecture/Set.js
@@ -12,11 +12,15 @@ class Set {
   }
 
   delete(val){
-
+    this.arr = this.arr.filter(x => x !== val)
   }
 
   has(val) {
     return this.arr.includes(val)
+  }
+
+  get size() {
+    return this.arr.length
   }
 }
 
@@ -27,12 +31,12 @@ s.add(1)
 s.add(1)
 console.log('s should have 5 members and actually has:', s.size)
 
-console.log('s should contain 5: ', s.has(5))
+console.log('s should contain 5:', s.has(5))
 
 s.add(6)
 console.log('s should contain 6:', s.has(6))
-console.log('s should have 6 members and actually has:', s.size())
+console.log('s should have 6 members and actually has:', s.size)
 
 s.delete(6)
-console.log('s should not contain 6:', s.has(6))
-console.log('s should have 5 members and actually has:', s.size())
+console.log('s should no longer contain 6:', !s.has(6))
+console.log('s should have 5 members and actually has:', s.size)
