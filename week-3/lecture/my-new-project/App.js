12aa3524872b8fff08e6cfc255b29a06f2cacde2
Dave Moran <dave.moran118@gmail.com>
2021-05-24 17:15:57 -0400

    Look into proptypes

--- week-3/lecture/my-new-project/App.js
+++ week-3/lecture/my-new-project/App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Count, {num} from './Count.js';
+import Count from './Count.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Count count={num} />
+      <Count count={"0"} />
     </View>
   );
 }
--- week-3/lecture/my-new-project/Count.js
+++ week-3/lecture/my-new-project/Count.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import {StyleSheet, Text} from 'react-native'
+import PropTypes from 'prop-types'
 
 const styles = StyleSheet.create({
     text: {fontSize: 72}
 })
 
-export const num = 50
-
 const Count = props => (
     <Text style={styles.text}>{props.count}</Text>
 )
+
+Count.propTypes = {
+    count: PropTypes.number.isRequired
+}
 
 export default Count
